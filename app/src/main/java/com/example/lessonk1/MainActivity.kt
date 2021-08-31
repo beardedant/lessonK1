17eceb22c92a26fc749370a7f2d30a9265322249
ant <theant@mail.ru>
2021-08-31 15:32:15 +0400

    добавил кнопку и повесил на нее clickListener

--- app/src/main/java/com/example/lessonk1/MainActivity.kt
+++ app/src/main/java/com/example/lessonk1/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.lessonk1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var counter = 0
+        initView()
+
+        button.setOnClickListener {
+            counter ++
+            textView.text = "button clicked $counter times"
+        }
+    }
+
+    private fun initView() {
+        textView = findViewById<TextView>(R.id.text_view)
+        button = findViewById<Button>(R.id.button)
     }
 }
