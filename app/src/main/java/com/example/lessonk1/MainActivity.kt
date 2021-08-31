cdf993de58bcbcaffc27481178779403a4431006
ant <theant@mail.ru>
2021-08-31 15:59:45 +0400

    1. вынес clickListener в отдельную функция 2. вынес установку значения в поле textView в отдельную функцию 3. добавил сохранение состояния (saveInstanceState)

--- app/src/main/java/com/example/lessonk1/MainActivity.kt
+++ app/src/main/java/com/example/lessonk1/MainActivity.kt
@@ -1,29 +1,51 @@
 package com.example.lessonk1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var textView: TextView
     private lateinit var button: Button
 
+    private var counter = 0
+    private val COUNTER_KEY = "COUNTER"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var counter = 0
         initView()
 
-        button.setOnClickListener {
-            counter ++
-            textView.text = "button clicked $counter times"
-        }
+        initListener()
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(COUNTER_KEY, counter)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        counter = savedInstanceState.getInt(COUNTER_KEY)
+        setTextViewContent(counter)
+    }
+
+
     private fun initView() {
-        textView = findViewById<TextView>(R.id.text_view)
-        button = findViewById<Button>(R.id.button)
+        textView = findViewById(R.id.text_view)
+        button = findViewById(R.id.button)
+    }
+
+    private fun setTextViewContent(counter : Int) {
+        textView.text = "$counter"
+    }
+
+    private fun initListener() {
+        button.setOnClickListener {
+            counter++
+            setTextViewContent(counter)
+        }
     }
 }
